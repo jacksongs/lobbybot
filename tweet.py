194efb814f5da68576f316c43a4c06238d807ee1
jacksongs <jacksongs@gmail.com>
2016-01-18 18:55:08 +1100

    Updated key names

--- tweet.py
+++ tweet.py
@@ -7,10 +7,10 @@ if os.path.isfile('variables.py') == True:
 	auth = tweepy.OAuthHandler(variables.CONSUMER_KEY, variables.CONSUMER_SECRET)
 	auth.set_access_token(variables.ACCESS_KEY, variables.ACCESS_SECRET)
 else:
-	ck = os.environ['CONSUMER_KEY']
-	cs = os.environ['CONSUMER_SECRET']
-	ak = os.environ['ACCESS_KEY']
-	ase = os.environ['ACCESS_SECRET']
+	ck = os.environ['MORPH_TWCKEY']
+	cs = os.environ['MORPH_TWCSEC']
+	ak = os.environ['MORPH_TWAKEY']
+	ase = os.environ['MORPH_TWASEC']
 	auth = tweepy.OAuthHandler(ck, cs)
 	auth.set_access_token(ak, ase)
 
